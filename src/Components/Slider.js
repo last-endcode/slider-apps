34f322c5d259d913aa6cc06103bb21f361afdeed
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2021-05-21 13:02:36 +0700

    slider-app

---
+++ src/Components/Slider.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { HiChevronDoubleRight, HiChevronDoubleLeft } from 'react-icons/hi';
+import { reviewsItem } from './reviewsItem';
+
+function Slider() {
+  const [reviews, setReviews] = useState(reviewsItem);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = reviews.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, reviews]);
+
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
+
+  return (
+    <div className='hero'>
+      <div className='hero-banner'>
+        {reviews.map((images, imagesIndex) => {
+          let position = 'nextSlide';
+
+          if (imagesIndex === index) {
+            position = 'activeSlide';
+          }
+
+          if (
+            imagesIndex === index - 1 ||
+            (index === 0 && imagesIndex === reviews.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article className={position}>
+              <img src={images.img} className='pic' alt='' />
+            </article>
+          );
+        })}
+
+        <button className='prev' onClick={() => setIndex(index - 1)}>
+          <HiChevronDoubleLeft />
+        </button>
+        <button className='next' onClick={() => setIndex(index + 1)}>
+          <HiChevronDoubleRight />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Slider;
---
+++ src/Components/reviewsItem.js
@@ -0,0 +1,17 @@
+export const reviewsItem = [
+  {
+    id: 1,
+    name: 'Welcome__',
+    img: 'https://images.pexels.com/photos/462024/pexels-photo-462024.jpeg',
+  },
+  {
+    id: 2,
+    name: 'explore our',
+    img: 'https://images.pexels.com/photos/2835562/pexels-photo-2835562.jpeg',
+  },
+  {
+    id: 3,
+    name: 'all nature',
+    img: 'https://images.pexels.com/photos/35600/road-sun-rays-path.jpg',
+  },
+];
